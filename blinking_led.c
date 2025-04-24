d609fdee6d15b071851b59cb3aa6bbabc9ae2680
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-04-24 19:12:07 +0330

    Refactor blinking LED program for clarity and consistency and fixed the not blinking problem

--- blinking_led.c
+++ blinking_led.c
@@ -1,15 +1,18 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define F_CPU 16000000UL // Make sure to define your clock frequency if you use _delay_ms()
+
 int main(void)
 {
-    DDRA |= (1 << PA0);
+	DDRA |= (1 << PA0); // Set PA0 as output
 
-    while (1)
-    {
-        PORTA |= (1 << PA0);
-        _delay_ms(500);
-        PORTA |= (0 << PA0);
-        _delay_ms(500);
-    }
+	while (1)
+	{
+		PORTA |= (1 << PA0); // Turn ON the LED (set PA0 high)
+		_delay_ms(500);
+
+		PORTA &= ~(1 << PA0); // Turn OFF the LED (set PA0 low)
+		_delay_ms(500);
+	}
 }
