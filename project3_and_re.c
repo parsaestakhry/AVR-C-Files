45db0a66b0c91c85deb6ab867b1b6ac59d586da6
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-04-25 12:34:15 +0330

    Refactor formatting and spacing in LED control program

--- project3_and_re.c
+++ project3_and_re.c
@@ -3,30 +3,28 @@
 #include <delay.h>
 #include <math.h>
 
-
 void main(void)
 {
-int  i;
-//unsigned char x[]={0X01,0X03,0X07,0X0F,0X1F,0X3F,0X7F,0XFF};
+      int i;
+      // unsigned char x[]={0X01,0X03,0X07,0X0F,0X1F,0X3F,0X7F,0XFF};
 
-DDRA=0XFF;
+      DDRA = 0XFF;
 
-while (1)
+      while (1)
       {
-      for (i=0;i<8;i++)
-      {
-       PORTA= (pow(2,i))-1;
-       //PORTA= (1<<i)-1
-       //PORTA= x[i];
-       delay_ms(40);
-      }
-      for (i=7;i>=0;i--)
-      {
-       PORTA= (pow(2,i))-1;
-       //PORTA= (1<<i)-1
-       //PORTA= x[i];
-       delay_ms(40);
-      }
-
+            for (i = 0; i < 8; i++)
+            {
+                  PORTA = (pow(2, i)) - 1;
+                  // PORTA= (1<<i)-1
+                  // PORTA= x[i];
+                  delay_ms(40);
+            }
+            for (i = 7; i >= 0; i--)
+            {
+                  PORTA = (pow(2, i)) - 1;
+                  // PORTA= (1<<i)-1
+                  // PORTA= x[i];
+                  delay_ms(40);
+            }
       }
 }
