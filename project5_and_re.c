6f94192431a5c2bc497ca8e1c040df6c5fa2db6b
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-04-25 19:00:14 +0330

    Refactor code formatting and spacing in LED control program

--- project5_and_re.c
+++ project5_and_re.c
@@ -4,23 +4,20 @@
 
 void main(void)
 {
- int i;
- unsigned char x[]={0X3F,0X06,0X5B,0X4F,0X66,0X6D,0X7D,0X07,0X7F,0X6F};
- DDRA=0XFF;
-while (1)
+      int i;
+      unsigned char x[] = {0X3F, 0X06, 0X5B, 0X4F, 0X66, 0X6D, 0X7D, 0X07, 0X7F, 0X6F};
+      DDRA = 0XFF;
+      while (1)
       {
-      for (i=0;i<10;i++)
-      {  
-       PORTA= x[i];
-        delay_ms(40);
-      } 
-       for (i=9;i>=0;i--)
-      {
-       PORTA= x[i]; 
-       delay_ms(40);
+            for (i = 0; i < 10; i++)
+            {
+                  PORTA = x[i];
+                  delay_ms(40);
+            }
+            for (i = 9; i >= 0; i--)
+            {
+                  PORTA = x[i];
+                  delay_ms(40);
+            }
       }
-      } 
-     
-
-
 }
