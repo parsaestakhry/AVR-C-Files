b276262ccc040193fe3a3f13b4d5152c4f7ba523
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-05-29 10:46:15 +0330

    Refactor comments for clarity in LCD control program

--- finals/LCD.c
+++ finals/LCD.c
@@ -1,13 +1,20 @@
 #include <mega16.h>
 // Alphanumeric LCD Module functions
+// begining of the assembly code 
 #asm
+// now the lcd module is connected to the port B
 .equ __lcd_port = 0x18;
 #endasm
+// including the header files
 #include <lcd.h>
 #include <delay.h>
+// main function
 void main(void)
 {
+
+    // loop variable init
     int i;
+    // amount of characters inside the lcd
     lcd_init(16);
     while (1)
     {
