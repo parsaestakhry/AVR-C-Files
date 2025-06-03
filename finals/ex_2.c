4f6cda1c9154e69ed9c99972e548971a4d5760f2
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-06-03 13:23:22 +0330

    Fix delays in traffic signal control and declare function prototype

--- finals/ex_2.c
+++ finals/ex_2.c
@@ -1,6 +1,6 @@
 #include <mega32.h>
 #include <delay.h>
-
+void trafficsignal(void);
 void main(void)
 {
     // PINS OF 0,1,2 OF PORTA IS OUTPUT
@@ -18,12 +18,13 @@ void trafficsignal(void)
     PORTA.0 = 1;
     PORTA.1 = 0;
     PORTA.2 = 0;
-    delay_ms(15000)
-        PORTA.0 = 0;
+    delay_ms(150);
+    PORTA.0 = 0;
     PORTA.1 = 1;
     PORTA.2 = 0;
-    delay_ms(15000)
-        PORTA.0 = 0;
+    delay_ms(150);
+    PORTA.0 = 0;
     PORTA.1 = 0;
     PORTA.2 = 1;
+    delay_ms(150);
 }
